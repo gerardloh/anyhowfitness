0c44f56dd4a4c4827b041c962385fde41e9d0f56
Jezelei <jezelei.work@gmail.com>
2023-10-30 19:46:48 +0800

    hi
    updated the home.html (bloghome)

--- public/js/home.js
+++ public/js/home.js
@@ -13,9 +13,9 @@ const createBlog = (blog) => {
     blogSection.innerHTML += `
     <div class="blog-card">
         <img src="${data.bannerImage}" class="blog-image" alt="">
-        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <h2 class="blog-title">${data.title.substring(0, 100) + '...'}</h2>
         <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
-        <a href="/${blog.id}" class="btn dark">read</a>
+        <a href="/${blog.id}" class="btn dark">READ</a>
     </div>
     `;
 }
